b5efc5a4a6d31bb96cce04a25682883414c17535
Azat <likeabro@mail.ru>
2017-04-20 23:24:14 +0300

    first commit

---
+++ Cursach/DatalinkLayer/CanalHandler.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+namespace Cursach.DatalinkLayer
+{
+    public class CanalHandler
+    {
+
+        private byte[] sFrame; //информационный кадр, который в случае ошибки передается заново
+
+        //ссылки на другие уровни
+        public PhysicalLayer.ComHandler ComManager { get; set; }
+        public Settings FormsManager { get; set; }
+
+        /// <summary>
+        /// Обрабатывает полученный кадр
+        /// </summary>
+        /// <param name="frame">Закодированный кадр</param>
+        public void RecieveFrame(byte[] frame)
+        {
+                switch (frame[0])
+                {
+                    case 0x25: //25h - начало передачи файла (BEGIN)
+                        string fileName = Encoding.Unicode.GetString(frame, 1, frame.Length - 1);
+                        string size = Encoding.Unicode.GetString(frame, 1, frame.Length - 1);;
+                        FormsManager.SavePrompt(fileName, size);
+                        break;
+                    case 0x18: //18h - отмена передачи файла (CANCEL)
+                        FormsManager.TransmissionCancel();
+                        break;
+                    case 0x6: //06h - положительная квитанция (ACK)
+                        SendData();
+                        break;
+                    case 0x14: //14h - отрицательная квитанция (NAK)
+                        SendAgain();
+                        break;
+                    case 0x40: //40h - информационный кадр (DATA)
+                        if (Check(frame))
+                        {
+                            byte[] rFrame = Decode(frame);
+                            FormsManager.WriteToFile(rFrame);
+                            Acknowledge();
+                        }
+                        else
+                        {
+                            NotAcknowledge();
+                        }
+                        break;
+                    case 0x4: //04h - конец файла (EOF)
+                        FormsManager.ReceiveSuccess();
+                        break;
+                }
+        }
+
+        /// <summary>
+        /// Отправляет кадр начала передачи
+        /// </summary>
+        /// <param name="fileName">Имя файла</param>
+        public void SendFile(string fileName, string filesize)
+        {
+            byte[] BEGIN = new byte[Encoding.Unicode.GetByteCount(fileName + filesize +"\n"+"Размер файла:") + 1];
+            BEGIN[0] = 0x25;
+            string inf = fileName + "\n" + "Размер файла:" + filesize;
+           // Encoding.Unicode.GetBytes(fileName, 0, fileName.Length, BEGIN, 1);
+           Encoding.Unicode.GetBytes(inf, 0, inf.Length, BEGIN, 1);
+            ComManager.WriteToCom(BEGIN);
+        }
+     /*    public void SendFileSize(string fileName, string filesize)
+        {
+
+            byte[] BEGIN = new byte[Encoding.Unicode.GetByteCount(fileName) + 1];
+            BEGIN[0] = 0x25;
+            Encoding.Unicode.GetBytes(filesize, 0, filesize.Length, BEGIN, 1);
+            ComManager.WriteToCom(BEGIN);
+
+        }*/
+        /// <summary>
+        /// Отправляет отрицательную квитанцию
+        /// </summary>
+        private void NotAcknowledge()
+        {
+            byte[] NAK = { 0x14 };
+            ComManager.WriteToCom(NAK);
+        }
+
+        public PhysicalLayer.PortState Connect(string portName, string baudRate, string parity, string dataBits, string stopBits)
+        {
+            PhysicalLayer.PortState portState = ComManager.OpenCom(portName, baudRate, parity, dataBits, stopBits);
+            ComManager.SendDtr();
+            if (ComManager.IsConnected)
+            {
+                return PhysicalLayer.PortState.Connected;
+            }
+            return portState;
+        }
+
+        public void NotConnected()
+        {
+            ComManager.CloseCom();
+            FormsManager.ConnectFail();
+        }
+
+        public void Connected()
+        {
+            FormsManager.ConnectSuccess();
+        }
+
+        /// <summary>
+        /// Отправляет заново информационный кадр
+        /// </summary>
+        private void SendAgain()
+        {
+            ComManager.WriteToCom(sFrame);
+        }
+
+        /// <summary>
+        /// Отправляет информационный кадр или EOF
+        /// </summary>
+        private void SendData()
+        {   
+            byte[] DATA = new byte[64];
+            int bytesRead = FormsManager.ReadFromFile(DATA);
+            if (bytesRead > 0)
+            {
+                Array.Resize(ref DATA, bytesRead);
+                sFrame = Encode(DATA);
+                ComManager.WriteToCom(sFrame);
+            }
+            else
+            {
+                byte[] EOF = { 0x4 };
+                ComManager.WriteToCom(EOF);
+                FormsManager.SendSuccess();
+            }
+        }
+
+        /// <summary>
+        /// Отправляет положительную квитанцию
+        /// </summary>
+        public void Acknowledge()
+        {
+            byte[] ACK = { 0x6 };
+            ComManager.WriteToCom(ACK);
+        }
+
+        /// <summary>
+        /// Отправляет кадр отмены передачи
+        /// </summary>
+        public void Abort()
+        {
+            byte[] CANCEL = { 0x18 }; 
+            ComManager.WriteToCom(CANCEL);
+        }
+
+        /// <summary>
+        /// Проверяет полученный кадр на ошибки
+        /// </summary>
+        /// <param name="bytes">Закодированный кадр</param>
+        /// <returns></returns>
+        private bool Check(byte[] bytes)
+        {
+            bool check = true;
+            int length = bytes.Length;
+            byte[] mid = new byte[length];
+            for (int i = 1; i < length; i++) //начинаем с единицы из-за 0 байта с кодом 
+            {
+                byte[] tmp = toarr(bytes[i]);
+                byte[] ham = new byte[7];
+                for (int a = 0; a < 7; a++)
+                {
+                    ham[a] = tmp[a + 1];
+                }
+                if (!HamCheck(ham))
+                {
+                    check = false;
+                    break;
+                }
+            }
+            return check;
+        }
+
+        /// <summary>
+        /// Декодирует полученный кадр
+        /// </summary>
+        /// <param name="bytes">Закодированный кадр</param>
+        /// <returns></returns>
+        private byte[] Decode(byte[] bytes)
+        {
+            int length = bytes.Length;
+            List<byte[]> tmpBytes = new List<byte[]>();
+            byte[] mid = new byte[length];
+            for (int i = 1; i < length; i++) //начинаем с единицы из-за 0 байта с кодом 
+            {
+                byte[] tmp = toarr(bytes[i]);
+                byte[] ham = new byte[7];
+                for (int a = 0; a < 7; a++)
+                {
+                    ham[a] = tmp[a + 1];
+                }
+                byte[] halfInBits = UnHam(ham);
+                tmpBytes.Add(halfInBits);
+            }
+            length--;
+            //соединяем половинки в целые байты 
+            byte[][] FullBytes = new byte[length / 2][];
+            byte[] nul = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < length / 2; i++)
+            {
+                FullBytes[i] = ret(nul, 8);
+            }
+            byte[] rez;
+            int add = 0;
+            int x = 0;
+            for (int c = 0; c < length; c++)
+            {
+                for (int a = 0; a < 4; a++)
+                {
+                    FullBytes[x][a + add] = tmpBytes[c][a];
+                    if (a == 3 && add != 4)
+                    {
+                        c++;
+                        add = 4;
+                        a = -1;
+                    }
+                }
+                x++;
+                add = 0;
+            }
+
+            //преобразуем из псевдодвоичного в нормальный вид 
+
+            rez = new byte[length / 2];
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                rez[i] = tobyte(FullBytes[i]);
+            }
+            return rez;
+        }
+
+        /// <summary>
+        /// Кодирует пересылаемый кадр
+        /// </summary>
+        /// <param name="bytes">Исходный кадр</param>
+        /// <returns></returns>
+        private byte[] Encode(byte[] bytes)
+        {
+            int size = bytes.Length;
+
+            byte[][] FullByteArray;
+            byte[][] HalfByteArray;
+            byte[][] CodeByteArray;
+            byte[] exmp = new byte[4];
+            byte[][] mid;
+            byte[] result;
+            byte[] nul = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] rez = new byte[2 * size];
+            FullByteArray = new byte[size][]; //массив для записи считанных байтов в псевдодвоичном виде (ПДД) 
+            HalfByteArray = new byte[size * 2][]; //массив для записи байтов в ПДД, разделенных на половинки 
+            CodeByteArray = new byte[2 * size][]; //массив для записи байтов в ПДД, закодированных кодом хэмминга 
+            mid = new byte[2 * size][]; //массив для хранения промежуточных данных 
+            result = new byte[(2 * size) + 1]; //лишний байт для кода, массив для хранения байтов, готовых для отправки 
+            for (int a = 0; a < size; a++)
+            {
+                FullByteArray[a] = toarr(bytes[a]);
+            }
+            //делим на блоки по 4 байта 
+            int b = 0;
+            int add = 0;
+            for (int a = 0; a < size; a++)
+            {
+                for (int x = 0; x < 4; x++)
+                {
+                    exmp[x] = FullByteArray[a][x + add];
+                    if (x == 3 && add != 4)
+                    {
+                        HalfByteArray[b] = ret(exmp, 4);
+                        b++;
+                        add = 4;
+                        x = -1;
+                    }
+                }
+                HalfByteArray[b] = ret(exmp, 4);
+                add = 0;
+                b++;
+            }
+            //кодируем кодом Хэмминга 
+            for (int a = 0; a < 2 * size; a++)
+            {
+                CodeByteArray[a] = Ham(HalfByteArray[a]);
+            }
+            //преобразуем в байты для передачи 
+            //суть преобразования - из массивов по 7 знаков (7битных чисел) сделать массивы по 8 знаков, дописав ноль в начале 
+            for (int a = 0; a < 2 * size; a++)
+            {
+                mid[a] = ret(nul, 8);
+            }
+            for (int a = 0; a < 2 * size; a++)
+            {
+                for (int c = 0; c < 7; c++)
+                {
+                    mid[a][c + 1] = CodeByteArray[a][c];
+                }
+            }
+            result[0] = 0x40;
+            for (int a = 0; a < 2 * size; a++)
+            {
+                result[a + 1] = tobyte(mid[a]);
+            }
+
+            return result;
+        }
+
+        private bool HamCheck(byte[] b)
+        {
+            byte check;
+            check = sum(b[2], sum(b[4], b[6]));
+            if (check != b[0])
+                return false;
+            check = sum(b[2], sum(b[5], b[6]));
+            if (check != b[1])
+                return false;
+            check = sum(b[4], sum(b[5], b[6]));
+            if (check != b[3])
+                return false;
+            return true;
+        }
+
+        private byte[] UnHam(byte[] code)
+        {
+            byte[] rez = new byte[4];
+            rez[0] = code[2];
+            rez[1] = code[4];
+            rez[2] = code[5];
+            rez[3] = code[6];
+            return rez;
+        }
+
+
+        private byte invert(byte b)
+        {
+            if (b == 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        private byte sum(byte a, byte b)
+        {
+            if (a == 0 && b == 0 || a == 1 && b == 1)
+                return 0;
+            else
+                return 1;
+        }
+
+        private byte[] ret(byte[] b, int n)
+        {
+            byte[] x = new byte[n];
+            for (int i = 0; i < 4; i++)
+            {
+                x[i] = b[i];
+            }
+            return x;
+        }
+
+        private byte[] toarr(int n)
+        {
+            byte[] rez = new byte[8];
+            int m = 128;
+            byte s;
+            for (int i = 0; i < 8; i++)
+            {
+                s = Convert.ToByte(n / m);
+                if (s != 0)
+                {
+                    n = n - m;
+                    rez[i] = s;
+
+                }
+                m = Convert.ToByte(m / 2);
+            }
+            return rez;
+        }
+
+        private byte tobyte(byte[] b)
+        {
+            byte rez = 0;
+            int m = 1;
+            for (int i = 7; i > -1; i--)
+            {
+                rez = Convert.ToByte(rez + (m * b[i]));
+                m = m * 2;
+            }
+            return rez;
+        }
+
+        private byte[] Ham(byte[] b)
+        {
+            byte[] code = new byte[7];
+            code[0] = 0;
+            code[1] = 0;
+            code[3] = 0;
+            code[2] = b[0];
+            code[4] = b[1];
+            code[5] = b[2];
+            code[6] = b[3];
+            code[0] = sum(sum(code[0], code[2]), sum(code[4], code[6]));
+            code[1] = sum(sum(code[1], code[2]), sum(code[5], code[6]));
+            code[3] = sum(sum(code[3], code[4]), sum(code[5], code[6]));
+
+            return code;
+        }
+
+    }
+}
---
+++ Cursach/PhysicalLayer/ComHandler.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+
+namespace Cursach.PhysicalLayer
+{
+    /// <summary>
+    /// Определяет статус COM-порта
+    /// </summary>
+    public enum PortState
+    {
+        Opened,
+        Occupied,
+        InvalidArgs,
+        Error,
+        Closed,
+        Connected
+    }
+
+    /// <summary>
+    /// Класс физического уровня
+    /// </summary>
+    public class ComHandler
+    {
+        private byte[] rFrame;
+        private int count = 0;
+        private int size = 0;
+
+        private SerialPort serialPort;
+
+        public bool IsConnected { get; private set; } //Статус соединения
+
+        // Ссылки на другие уровни
+        public Settings FormsManager { get; set; }
+        public DatalinkLayer.CanalHandler CanalManager { get; set; }
+
+        public ComHandler()
+        {
+            serialPort = new SerialPort();
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(ComPort_DataReceived);
+            serialPort.PinChanged += new SerialPinChangedEventHandler(ComPort_PinChanged);
+        }
+
+        ~ComHandler()
+        {
+            CloseCom();
+        }
+
+        /// <summary>
+        /// Возвращает сортированный список COM-портов
+        /// </summary>
+        public static string[] GetSortedPortNames()
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            Array.Sort(portNames);
+            return portNames;
+        }
+
+        /// <summary>
+        /// Открывает COM-порт с заданными параметрами
+        /// </summary>
+        public PortState OpenCom(string portName, string baudRate, string parity, string dataBits, string stopBits)
+        {
+            CloseCom();
+            try
+            {
+                serialPort.PortName = portName.Trim();
+                serialPort.BaudRate = int.Parse(baudRate.Trim());
+                serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), parity.Trim());
+                serialPort.DataBits = int.Parse(dataBits.Trim());
+                serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), stopBits.Trim());
+                serialPort.Encoding = Encoding.Unicode;
+                serialPort.Handshake = Handshake.None;
+                serialPort.ReadTimeout = 500;
+                serialPort.WriteTimeout = 500;
+                serialPort.Open();
+                return PortState.Opened;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return PortState.Occupied;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return PortState.InvalidArgs;
+            }
+            catch (Exception)
+            {
+                return PortState.Error;
+            }
+        }
+
+        public void SendDtr()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.DtrEnable = true;
+                if (serialPort.DsrHolding)
+                {
+                    IsConnected = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Закрывает COM-порт
+        /// </summary>
+        public void CloseCom()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                serialPort.DtrEnable = false;
+                IsConnected = false;
+                Thread.Sleep(100);
+            }
+        }
+
+        /// <summary>
+        /// Записывает кадр в COM-порт
+        /// </summary>
+        /// <param name="frame">Отправляемый кадр</param>
+        public void WriteToCom(byte[] frame)
+        {
+            byte[] sBuffer = new byte[frame.Length + 1];
+            sBuffer[0] = Convert.ToByte(frame.Length);
+            for (int i = 1; i < sBuffer.Length; i++)
+            {
+                sBuffer[i] = frame[i - 1];
+            }
+            serialPort.Write(sBuffer, 0, sBuffer.Length);
+        }
+
+        /// <summary>
+        /// Обработчик события, возникающего при получении данных с COM-порта
+        /// </summary>
+        private void ComPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (serialPort.BytesToRead > 0)
+            {
+                if (size == 0)
+                {
+                    size = serialPort.ReadByte();
+                    rFrame = new byte[size];
+                }
+                int bytesRead = serialPort.Read(rFrame, count, size - count);
+                count += bytesRead;
+                if (count == size)
+                {
+                    count = 0;
+                    size = 0;
+                    CanalManager.RecieveFrame(rFrame);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события, возникающего при изменении состояния пинов COM-порта
+        /// </summary>
+        private void ComPort_PinChanged(object sender, SerialPinChangedEventArgs e)
+        {
+            switch (e.EventType)
+            {
+                case SerialPinChange.Break:
+                    IsConnected = false;
+                    CanalManager.NotConnected();
+                    break;
+                case SerialPinChange.DsrChanged:
+                    if (serialPort.DsrHolding && !IsConnected)
+                    {
+                        IsConnected = true;
+                        CanalManager.Connected();
+                    }
+                    else if (!serialPort.DsrHolding && IsConnected)
+                    {
+                        IsConnected = false;
+                        CanalManager.NotConnected();
+                    }
+                    break;
+            }
+        }
+    }
+}
